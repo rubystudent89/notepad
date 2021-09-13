de288d646c3a78f2747a850d3a01284e60b22218
rubystudent <rubystudent@mail.ru>
2021-09-13 11:51:51 +0300

    Notepad v. 1.0, working

--- task.rb
+++ task.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Task < Post
 
     def initialize
@@ -7,8 +9,20 @@ class Task < Post
     end
 
     def read_from_console
+        puts "Что надо сделать?"
+        @text = STDIN.gets.chomp
+
+        puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ, например, 12.03.2020"
+        input = STDIN.gets.chomp
+
+        @due_date = Date.parse(input)
     end
 
     def to_strings
+        time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")}"
+
+        deadline = "Крайний срок: #{@due_date}"
+
+        return [deadline, @text, time_string]
     end
 end
